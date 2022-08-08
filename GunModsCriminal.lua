1dc73b9e590d782677b6a764348cef4ef1a128ac
dudeididntliterally <75636857+dudeididntliterally@users.noreply.github.com>
2022-08-08 17:19:17 -0400

    Create GunModsCriminal.lua

---
+++ GunModsCriminal.lua
@@ -0,0 +1,64 @@
+local Library = loadstring(game:HttpGet("https://raw.githubusercontent.com/bloodball/-back-ups-for-libs/main/kav"))()
+local Window = Library.CreateLib("Criminal Gun Mods (SWFL)", "DarkTheme")
+local HomeWindow = Window:NewTab("Home")
+local Home = HomeWindow:NewSection("Home")
+local OtherModsWindow = Window:NewTab("Other Mods")
+local OtherMods = OtherModsWindow:NewSection("Gun Mods #2")
+local AntiCheat = Window:NewTab("Anti Cheat")
+local ac = AntiCheat:NewSection("Anti Cheat Main")
+
+game:GetService("StarterGui"):SetCore("ChatMakeSystemMessage",{
+	Text = "Must Be Criminal For This To Work, ALSO PLEASE BYPASS ANTI CHEAT YOU WON'T BE ABLE TO USE IT WITHOUT BYPASS, DO NOT PRESS IF YOU ALREADY BYPASSED",
+	Color = Color3.fromRGB(207, 96, 36)
+})
+wait()
+game:GetService("StarterGui"):SetCore("ChatMakeSystemMessage",{
+	Text = "Must Have Pistol (only works for pistol for right now) also put the gun away for this to work",
+	Color = Color3.fromRGB(207, 96, 36)
+})
+wait()
+ac:NewButton("Click Me To Bypass Anti Cheat", "Anti Cheat", function()
+game.ReplicatedStorage.Remotes.SecureSettings:Destroy()
+end)
+wait()
+Home:NewButton("Mod FireRate", "Home", function()
+local m = require(game:GetService("Players").LocalPlayer.Backpack.M9.Setting)
+    m.FireRate = 0.01
+end)
+
+Home:NewButton("Mod Ammo", "Home", function()
+local m = require(game:GetService("Players").LocalPlayer.Backpack.M9.Setting)
+    m.Ammo = 999999
+    m.LimitedAmmoEnabled = false
+end)
+
+Home:NewButton("Mod Reload Time", "Home", function()
+local m = require(game:GetService("Players").LocalPlayer.Backpack.M9.Setting)
+    m.ReloadTime = 0.01
+end)
+
+Home:NewButton("Mod Extra Ammo", "Home", function()
+local m = require(game:GetService("Players").LocalPlayer.Backpack.M9.Setting)
+    m.AmmoPerMag = 999999
+    m.LimitedAmmoEnabled = false
+end)
+
+Home:NewButton("Mod Spread", "Home", function()
+local m = require(game:GetService("Players").LocalPlayer.Backpack.M9.Setting)
+    m.Spread = 0.01
+end)
+
+Home:NewButton("Mod Range", "Home", function()
+local m = require(game:GetService("Players").LocalPlayer.Backpack.M9.Setting)
+    m.Range = 9000000
+end)
+
+OtherMods:NewButton("Mod Recoil", "Other Mods", function()
+local m = require(game:GetService("Players").LocalPlayer.Backpack.M9.Setting)
+    m.Recoil = 0.01
+end)
+
+OtherMods:NewButton("Fully Auto", "Other Mods", function()
+local m = require(game:GetService("Players").LocalPlayer.Backpack.M9.Setting)
+    m.Auto = true
+end)
